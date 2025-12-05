a49db5211e8cddeca1c98c995c76623cb494565a
Anand6262 <pyexpert62@gmail.com>
2025-12-05 17:20:57 +0530

    chanegs

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Formik, Form, Field } from "formik";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { createApi } from "./services/dummyApi";
 
 function Post() {
   const initialValues = {
@@ -10,24 +11,19 @@ function Post() {
     userId: "",
   };
 
+
   const handleSubmit = async (values, { resetForm }) => {
     const { title, body, userId } = values;
-    const payload = { 
-      userId: title, body, userId,
+    const payload = {
+      userId: userId,
       title: title,
       body: body,
       company: 'SRS Live Technologies Private Limited'
-     };
+    };
 
     try {
-      const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-      });
-
-      const data = await response.json();
-      console.log("API Response:", data);
+      const response = await createApi(payload)
+      // console.log("API Response:", response);
 
       toast.success("Data created successfully!", {
         position: "top-right",
@@ -48,7 +44,7 @@ function Post() {
   return (
     <div className="max-w-md mx-auto mt-10 p-4">
       <ToastContainer />
-      <h2 className="text-lg font-bold mb-4">POST API Form</h2>
+      <h2 className="text-3xl font-semibold mb-4">POST API Form</h2>
 
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         <Form className="space-y-3">
@@ -70,7 +66,7 @@ function Post() {
 
           <button
             type="submit"
-            className="bg-blue-600 text-white p-2 rounded w-full"
+            className="bg-teal-600 text-white p-2 rounded w-full"
           >
             Submit
           </button>
